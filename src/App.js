b40cfee7f7954a6eb60ba1dc389ea26035355281
MitjaDragan <mitja.dragan@gmail.com>
2024-08-20 12:42:30 +0200

    Start of the portfolio project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';  // Assuming you have a Navbar component
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* Other components or content can go here */}
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is the main content area.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">Mitja Dragan</div>
+            <ul className="navbar-links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div className="navbar-button">
+                <a href="#contact">Hire Me</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
