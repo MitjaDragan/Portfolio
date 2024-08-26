4c56c516c3611f04f4f9199e06e19f9752c50d28
MitjaDragan <mitja.dragan@gmail.com>
2024-08-26 13:50:06 +0200

    Added responsive design to entire project

--- src/components/Flipbox.js
+++ src/components/Flipbox.js
@@ -4,20 +4,42 @@ import './FlipBox.css';
 const FlipBox = () => {
   return (
     <div className="flip-box-container">
-      {['Box 1', 'Box 2', 'Box 3'].map((box, index) => (
-        <div key={index} className="flip-box">
-          <div className="flip-box-inner">
-            <div className="flip-box-front">
-              <h2>{box}</h2>
-              <p>Hover to flip</p>
-            </div>
-            <div className="flip-box-back">
-              <h2>{box} Back</h2>
-              <p>More information here</p>
-            </div>
+      <div className="flip-box">
+        <div className="flip-box-inner">
+          <div className="flip-box-front">
+            <h2>Box 1</h2>
+            <p>Hover to flip</p>
+          </div>
+          <div className="flip-box-back">
+            <h2>Box 1 Back</h2>
+            <p>More information here</p>
           </div>
         </div>
-      ))}
+      </div>
+      <div className="flip-box">
+        <div className="flip-box-inner">
+          <div className="flip-box-front">
+            <h2>Box 2</h2>
+            <p>Hover to flip</p>
+          </div>
+          <div className="flip-box-back">
+            <h2>Box 2 Back</h2>
+            <p>More information here</p>
+          </div>
+        </div>
+      </div>
+      <div className="flip-box">
+        <div className="flip-box-inner">
+          <div className="flip-box-front">
+            <h2>Box 3</h2>
+            <p>Hover to flip</p>
+          </div>
+          <div className="flip-box-back">
+            <h2>Box 3 Back</h2>
+            <p>More information here</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,73 +4,97 @@ import './Navbar.css';
 
 import LogoLightTheme from '../assets/images/LogoLightTheme.png';
 import LogoDarkTheme from '../assets/images/LogoDarkTheme.png';
+import LogoLightThemeSmall from '../assets/images/LogoLightThemeSmall.png';
+import LogoDarkThemeSmall from '../assets/images/LogoDarkThemeSmall.png';
 import SunIcon from '../assets/images/Sun.png';
 import MoonIcon from '../assets/images/Moon.png';
 
-
 function Navbar() {
-    const [theme, setTheme] = useState('dark');
-    const [transitioning, setTransitioning] = useState(false);
+  const [theme, setTheme] = useState('dark');
+  const [transitioning, setTransitioning] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 420);
 
-    const toggleTheme = () => {
-      setTransitioning(true);
+  const toggleTheme = () => {
+    setTransitioning(true);
+    setTimeout(() => {
+      setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+      setTransitioning(false);
+    }, 150);
+  };
 
-      setTimeout(() => {
-          setTheme(prevTheme => prevTheme === 'dark' ? 'light' : 'dark');
-          setTransitioning(false);
-      }, 150);  // Match this duration with the CSS transition duration
+  const handleToggle = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  useEffect(() => {
+    document.body.className = theme + '-theme';
+
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 420);
     };
-    
-    useEffect(() => {
-      document.body.className = theme + '-theme';
-    }, [theme]);
 
-    return (
-        <nav className={`navbar navbar-expand-lg fixed-top ${theme === 'dark' ? 'navbar-dark bg-dark' : 'bg-light navbar-light'}`}>
-            <div className="container">
-              <Link to="/">
-                <a className="navbar-brand"
-                  ><img
-                    src={theme === 'light' ? LogoLightTheme : LogoDarkTheme}
-                    alt="Logo"
-                    draggable="false"
-                    height="60"
-                /></a>
-              </Link>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav ms-auto align-items-center">
-                    <li className="nav-item">
-                      <a className="nav-link mx-2" to="/"><i className="fas fa-plus-circle pe-2"></i>About me</a>
-                    </li>
-                    <li className="nav-item">
-                      <Link to="/projects">
-                        <a className="nav-link mx-2" href="#!"><i className="fas fa-bell pe-2"></i>Projects</a>
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link mx-2" href="#!"><i className="fas fa-heart pe-2"></i>Certificates</a>
-                    </li>
+    window.addEventListener('resize', handleResize);
 
-                    <li className="nav-item ms-3">
-                      <button onClick={toggleTheme} className="theme-toggle-btn">
-                        <img
-                            src={theme === 'dark' ? SunIcon : MoonIcon}
-                            alt="Toggle Button"
-                            height="30"
-                            className={transitioning ? 'fade-out' : 'fade-in'}
-                        />
-                      </button>
-                    </li>
-                    <li className="nav-item ms-3">
-                      <button className={`btn btn-rounded ${theme === 'dark' ? 'btn-dark-theme' : 'btn-light-theme'}`}>
-                          Hire me
-                      </button>
-                    </li>
-                  </ul>                  
-                </div>
-            </div>
-        </nav>
-    );
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [theme]);
+
+  return (
+    <nav className={`navbar navbar-expand-lg fixed-top ${theme === 'dark' ? 'navbar-dark bg-dark' : 'bg-light navbar-light'}`}>
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          <img
+            src={theme === 'light'
+              ? (isSmallScreen ? LogoLightThemeSmall : LogoLightTheme)
+              : (isSmallScreen ? LogoDarkThemeSmall : LogoDarkTheme)}
+            alt="Logo"
+            draggable="false"
+            height="60"
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleToggle}
+          aria-controls="navbarSupportedContent"
+          aria-expanded={!isCollapsed}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link mx-2"><i className="fas fa-plus-circle pe-2"></i>About me</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/projects" className="nav-link mx-2"><i className="fas fa-bell pe-2"></i>Projects</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/certificates" className="nav-link mx-2"><i className="fas fa-heart pe-2"></i>Certificates</Link>
+            </li>
+            <li className="nav-item ms-3">
+              <button onClick={toggleTheme} className="theme-toggle-btn">
+                <img
+                  src={theme === 'dark' ? SunIcon : MoonIcon}
+                  alt="Toggle Button"
+                  height="30"
+                  className={transitioning ? 'fade-out' : 'fade-in'}
+                />
+              </button>
+            </li>
+            <li className="nav-item ms-3">
+              <button className={`btn btn-rounded ${theme === 'dark' ? 'btn-dark-theme' : 'btn-light-theme'}`}>
+                Hire me
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
