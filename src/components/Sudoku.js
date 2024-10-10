bc0e1d174b1781732fcb7b64aedc5ffa9d3f1832
MitjaDragan <mitja.dragan@gmail.com>
2024-10-10 09:25:57 +0200

    Working sudoku generator component (optimized)

--- src/components/Sudoku.js
+++ src/components/Sudoku.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sudoku.css';
 
-const generateSudokuPuzzle = () => {
-  return [
-    [5, 3, null, null, 7, null, null, null, null],
-    [6, null, null, 1, 9, 5, null, null, null],
-    [null, 9, 8, null, null, null, null, 6, null],
-    [8, null, null, null, 6, null, null, null, 3],
-    [4, null, null, 8, null, 3, null, null, 1],
-    [7, null, null, null, 2, null, null, null, 6],
-    [null, 6, null, null, null, null, 2, 8, null],
-    [null, null, null, 4, 1, 9, null, null, 5],
-    [null, null, null, null, 8, null, null, 7, 9],
-  ];
-};
+import SudokuGenerator from './SudokuGenerator';
+
+const N = 9;
+const K = 40;
 
 const Sudoku = () => {
-  const [puzzle, setPuzzle] = useState(generateSudokuPuzzle());
+  const [puzzle, setPuzzle] = useState([]);
+
+  const generateNewPuzzle = () => {
+    const sudoku = new SudokuGenerator(N, K);
+    const generatedPuzzle = sudoku.fillValues();
+    setPuzzle(generatedPuzzle);
+  };
+
+  useEffect(() => {
+    generateNewPuzzle();
+  }, []);
 
   const handleInputChange = (row, col, value) => {
     const newPuzzle = [...puzzle];
@@ -30,24 +31,24 @@ const Sudoku = () => {
 
   return (
     <div class="container">
-        <div className="sudoku-grid">
+      <div className="sudoku-grid">
         {puzzle.map((row, rowIndex) => (
-            <div key={rowIndex} className="sudoku-row">
+          <div key={rowIndex} className="sudoku-row">
             {row.map((cell, colIndex) => (
-                <input
+              <input
                 key={`${rowIndex}-${colIndex}`}
                 className="sudoku-cell"
                 type="text"
                 maxLength="1"
                 value={cell || ''}
                 onChange={(e) => handleInputChange(rowIndex, colIndex, e.target.value)}
-                disabled={cell !== null} // Disable pre-filled cells
-                />
+                disabled={cell !== null}
+              />
             ))}
-            </div>
+          </div>
         ))}
-        </div>
-        <button onClick={() => setPuzzle(generateSudokuPuzzle())}>New Puzzle</button>
+      </div>
+      <button onClick={generateNewPuzzle}>New Puzzle</button>
     </div>
   );
 };
