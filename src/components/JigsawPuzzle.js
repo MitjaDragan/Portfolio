3d860bae0bc510f88e8ad889429f24845ba3544a
MitjaDragan <mitja.dragan@gmail.com>
2024-11-07 11:38:59 +0100

    Updated imageMap for Hard level

--- src/components/JigsawPuzzle.js
+++ src/components/JigsawPuzzle.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DroppableArea from './DroppableArea';
 
+//<DroppableArea testMode={true} />
+
 function JigsawPuzzle() {
     return (
         <div className="jigsaw-puzzle">
