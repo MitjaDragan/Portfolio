f913e182ee11267069894e91718d58c6ddc77e1d
MitjaDragan <mitja.dragan@gmail.com>
2025-01-24 14:46:03 +0100

    More testing

--- src/components/Jigsaw Puzzle/BezierCurveGrid.js
+++ src/components/Jigsaw Puzzle/BezierCurveGrid.js
@@ -96,7 +96,21 @@ const BezierCurveGrid = ({ cellSize = 50, gridRows = 4, gridCols = 4 }) => {
   <path d="M -7,29 C 1,29,20,34,37,35,55,36,39,13,53,9,79,-5,67,30,79,30,101,30,124,34,126,28" 
         stroke="black" fill="transparent" strokeWidth="2" />
 </svg>
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="41.000000pt" height="51.000000pt" viewBox="0 0 41.000000 51.000000"
+ preserveAspectRatio="xMidYMid meet">
 
+<g transform="translate(0.000000,51.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+<path d="M142 430 c0 -14 2 -19 5 -12 2 6 2 18 0 25 -3 6 -5 1 -5 -13z"/>
+<path d="M132 380 c0 -14 2 -19 5 -12 2 6 2 18 0 25 -3 6 -5 1 -5 -13z"/>
+<path d="M122 316 l-2 -38 33 7 c17 4 42 9 54 12 24 6 47 -22 59 -70 l7 -29
+-46 7 c-39 6 -47 4 -56 -13 -6 -12 -11 -27 -10 -34 0 -7 5 0 11 15 10 28 12
+28 59 22 45 -6 49 -5 49 14 0 34 -33 91 -55 97 -11 3 -33 0 -49 -5 -40 -16
+-44 -14 -49 22 -4 30 -4 30 -5 -7z"/>
+<path d="M153 100 c0 -25 2 -35 4 -22 2 12 2 32 0 45 -2 12 -4 2 -4 -23z"/>
+</g>
+</svg>
     </div>
   );
 };
